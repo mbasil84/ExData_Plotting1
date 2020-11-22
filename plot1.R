5964777869e4fe785ddbb39d19536d0633b6527c
mbasil84 <mbasil84@hotmail.com>
2020-11-22 13:06:14 +0200

    Comments added

--- plot1.R
+++ plot1.R
@@ -1,9 +1,16 @@
+# we load the data from the file
 complete_data <- read.csv("household_power_consumption.txt", header=TRUE, sep=';', na.strings="?", stringsAsFactors=FALSE)
 
+# we subset the data to keep only the data we want to use
 my_data <- subset(complete_data, Date %in% c("1/2/2007","2/2/2007"))
 
+#we format the date
 my_data$Datetime <-as.POSIXlt.character(paste(my_data$Date,my_data$Time),format = "%d/%m/%Y %H:%M:%S")
 
+#we create the histogram
+hist(my_data$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', ylab="Frequency")
+
+#we create the png file
 dev.copy(png, file="plot1.png", height=480, width=480)
 
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,19 +1,24 @@
+# we load the data from the file
 complete_data <- read.csv("household_power_consumption.txt", header=TRUE, sep=';', na.strings="?", stringsAsFactors=FALSE)
 
+# we subset the data to keep only the data we want to use
 my_data <- subset(complete_data, Date %in% c("1/2/2007","2/2/2007"))
 
+#we format the date
 my_data$Datetime <-as.POSIXlt.character(paste(my_data$Date,my_data$Time),format = "%d/%m/%Y %H:%M:%S")
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,2), mar=c(4,4,2,1))
 
+#we plot the data
 plot(x=my_data$Datetime ,y=my_data$Global_active_power, type = 'l', xlab='',ylab = 'Global Active Pow er')
 plot(x=my_data$Datetime ,y=my_data$Voltage, type = 'l', xlab='datetime',ylab = 'Voltage')
 plot(x=my_data$Datetime,y=my_data$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
 lines(x=my_data$Datetime,y=my_data$Sub_metering_2, col='red')
 lines(x=my_data$Datetime,y=my_data$Sub_metering_3, col='blue')
-legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1, bty = "n")
+legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1)
 plot(x=my_data$Datetime ,y=my_data$Global_reactive_power, type = 'l', xlab='datetime',ylab = 'Global_reactive_power')
 
+#we create the png file
 dev.copy(png, file="plot4.png", height=480, width=480)
 
 dev.off()
